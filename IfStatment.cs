6ac7e6cf1b6f0f1d481b468c9887d460a2d27d1a
qrNasr <83606441+qrNasr@users.noreply.github.com>
2021-05-08 19:54:46 +0300

    Add files via upload

---
+++ IfStatment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const int month = 12;// declare constant value
+            const int py = 132;// declare constant value
+            const int weeks = 52;
+            const double pi = 3.14;
+            byte num1;
+            num1 = 5;
+            String strname; // declare variable
+            strname = "nasr";// assign variable value
+            bool b = true;
+            bool A = false;
+            double d = 123.45;
+            Console.WriteLine(month);
+            Console.WriteLine(py);
+            Console.WriteLine(weeks);
+            Console.WriteLine(pi);
+            Console.WriteLine(num1);
+            Console.WriteLine(strname);
+            Console.WriteLine(b);
+            Console.WriteLine(A);
+            Console.WriteLine(d);
+        }
+    }
+}
