c7c8d2babe8ffc064617e4793654da60ba5d92c6
qrNasr <83606441+qrNasr@users.noreply.github.com>
2021-05-11 22:18:00 +0300

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,11 @@
+
+userName = input("Please enter your name: ")
+times = int(input("how many times you want top print your name:"))
+
+for k in range(times):
+    print("Hello "+  userName)
+
+print('+++++++++++++++++++++++++++++')
+
+for k in range(10):
+     print("Hello")
